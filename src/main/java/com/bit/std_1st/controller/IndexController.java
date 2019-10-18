4c6116db6288b9e666b17ed7151e12bbd2d2caf8
SungBin <msb9876@naver.com>
2019-10-18 10:02:24 +0900

    first commit

---
+++ src/main/java/com/bit/std_1st/controller/IndexController.java
@@ -0,0 +1,35 @@
+package com.bit.std_1st.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/indextest2.do")
+	public void indextest() {
+		
+	}
+	
+	@RequestMapping("/blank_page")
+	public void blank_page() {
+		
+	}
+	
+	@RequestMapping("/cafeList.do")
+	public void blank0() {
+		
+	}
+	
+	@RequestMapping("/cafeDetail.do")
+	public void cafeDetail() {
+		
+	}
+	
+	@RequestMapping("/blank1.do")
+	public void blank1() {
+		
+	}
+	
+
+}
